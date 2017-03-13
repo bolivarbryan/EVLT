17aea74a014d9561aed483f7e3b6a46409baf7b1
Bryan <bolivarbryan@gmail.com>
2017-03-13 05:40:27 -0500

    TGP

---
+++ Pods/Target Support Files/TGPControls/TGPControls-umbrella.h
@@ -0,0 +1,13 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "TGPCamelLabels.h"
+#import "TGPCamelLabels7.h"
+#import "TGPControlsTicksProtocol.h"
+#import "TGPDiscreteSlider.h"
+#import "TGPDiscreteSlider7.h"
+
+FOUNDATION_EXPORT double TGPControlsVersionNumber;
+FOUNDATION_EXPORT const unsigned char TGPControlsVersionString[];
+
